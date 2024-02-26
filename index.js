118784d758bb52280745bfc4b77271f4176e35b3
AVTAR SINGH <avtarsingh@AVTARs-MacBook-Air.local>
2024-02-26 07:01:47 +0530

    Sockets Backend

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const users = [{}];
+
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
@@ -20,9 +22,32 @@ app.use(cors());
 connectdb();
 
 // Socket.io
-io.on('connection',(socket)=>{
-    console.log("New Connection",socket.id);
+io.on('connection', (socket) => {
+    console.log("New Connection");
+
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user;
+        console.log("User Joined", user);
+
+        socket.emit('welcome', { user: "admin", message: `Welcome ${user}` });
+        socket.broadcast.emit('userJoined', { user: "admin", message: `${users[socket.id]} Joined the Pad` });
+    });
+
+    socket.on('disconnect', () => {
+        console.log("User Disconnected", users[socket.id]);
+        socket.broadcast.emit('userLeft', { user: "admin", message: `${users[socket.id]} Left the Pad` });
+        
+    });
+
+    // ... other event listeners ...
+
+    // socket.on('contentChange',(data)=>{
+    //     console.log(data);
+    //     socket.broadcast.emit('contentChange',data);
+    // });
+
 });
+
 // Routes
 app.get('/',(req,res)=>{
     res.send("Server Running");
